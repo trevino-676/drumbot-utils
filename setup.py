585ecd3fe9c3169af2bba62848c04f64fd33a555
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-21 11:52:35 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="drumbot-utils",
+    packages=["drumbot-utils"],
+    version="0.1",
+    description="Funciones, metodos y clases que se usan de forma recurrente en los "
+                "proyectos de drumbot",
+    author="Luis Manuel Torres Trevino",
+    author_email="lmtorres123@icloud.com",
+    url="https://github.com/trevino-676/drumbot-utils",
+    download_url="https://github.com/trevino-676/drumbot-utils/tarball/0.1",
+    keywords=[],
+    classifiers=[],
+)
