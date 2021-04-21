2c60debd298de8f83d6e1a7ec81fa6adc5ff46b4
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-21 14:01:04 -0500

    Se agrega un long description

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from distutils.core import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="drumbot-utils",
     packages=["drumbot-utils"],
     version="0.1",
     description="Funciones, metodos y clases que se usan de forma recurrente en los "
                 "proyectos de drumbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Luis Manuel Torres Trevino",
     author_email="lmtorres123@icloud.com",
     url="https://github.com/trevino-676/drumbot-utils",
     download_url="https://github.com/trevino-676/drumbot-utils/tarball/0.1",
     keywords=[],
     classifiers=[],
+    install_requires=[
+        "pymongo"
+    ]
 )
